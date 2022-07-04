3ad0708e32cda8aa2ea2c97cd03b0b7ec1e2bb35
Devil Night <taolagiakaka@gmail.com>
2022-07-04 21:31:46 +0700

    update

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -50,6 +50,10 @@ namespace Ordering.API
             });
             services.AddMassTransitHostedService();
 
+            // General Configuration
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<BasketCheckoutConsumer>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
